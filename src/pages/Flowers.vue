2534b52caccba52384e40ae87206450948b53af7
Patrick <patrickmircea@gmail.com>
2025-12-05 11:33:20 +0200

    s

--- src/pages/Flowers.vue
+++ src/pages/Flowers.vue
@@ -1,12 +1,29 @@
 <script setup>
-import GreenFlower from '@/components/flowers/GreenFlower.vue'
+import GreenFlower from "@/flowers/GreenFlower.vue";
+import WaterTheFlowers from "@/flowers/WaterTheFlowers.vue";
+
 </script>
 
 <template>
     <h3>Growth</h3>
-    <hr>
+    <hr />
 
-    <GreenFlower />
-    <hr>
+    <div class="flex justify-between w-5/6">
+        <WaterTheFlower
+            v-for="flower in [0, 1, 2, 3]"
+            :key="flower"
+        ></WaterTheFlower>
+    </div>
+
+    <div class="flex justify-between w-5/6">
+        <GreenFlower v-for="flower in [0, 1, 2, 3]" :key="flower" />
+    </div>
+
+    <hr />
     <h3>Earth</h3>
 </template>
+
+<style>
+@import "tailwindcss";
+@import "bootstrap-icons";
+</style>
