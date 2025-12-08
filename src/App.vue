7924a83a9e81b24d91f4b070e04d96cae8f2580e
Patrick <patrickmircea@gmail.com>
2025-12-08 10:19:01 +0200

    s

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+
 </script>
 
 <template>
  <header>
-     <nav>
+     <nav class="flex gap-4 mb-4">
        <RouterLink to="/">Home</RouterLink>
        <RouterLink to="/flowers">Flowers</RouterLink>
+       <RouterLink to="/computed">Computed</RouterLink>
+       <RouterLink to="/store">Store Test</RouterLink>
+       <RouterLink to="/tasks">Tasks</RouterLink>
      </nav>
  </header>
 
